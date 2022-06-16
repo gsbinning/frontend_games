9ae26646612b93f63e8e4d741a43bd72cb20099d
Gugandeep Singh Binning <gugandeep.s@hotmail.com>
2022-06-16 14:41:49 +0100

    fixed_casing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Reviews from "./components/Reviews";
-import Categories from "./components/Categories";
+import Header from "./Components/Header";
+import Nav from "./Components/Nav";
+import Reviews from "./Components/Reviews";
+import Categories from "./Components/Categories";
 //import ReviewsFiltered from "./components/ReviewsFiltered";
 //import dataLoop from "./components/video/dataLoop.mp4";
-import SingleReview from "./components/SingleReview";
+import SingleReview from "./Components/SingleReview";
 
 function App() {
   return (
